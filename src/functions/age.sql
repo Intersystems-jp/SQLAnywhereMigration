26031cb3138c05e28b33c8d836094c20aee2b910
wolfman0719 <hsato0719@icloud.com>
2025-01-30 12:25:25 +0900

    Add files via upload

--- src/functions/age.sql
+++ src/functions/age.sql
@@ -11,7 +11,7 @@ declare @offset integer
 -- 年齢の計算
 --
 --////////////////////////////////////////////////////////////////////
-if ((month(@rymd)*100)+day(@rymd))<((month(@bymd)*100)+day(@rymd)) then set @offset=1
+if ((month(@rymd)*100)+day(@rymd))<((month(@bymd)*100)+day(@bymd)) then set @offset=1
 else set @offset=0
 set @age = datediff(YEAR,@bymd,@rymd) - @offset
 --////////////////////////////////////////////////////////////////////
@@ -21,6 +21,6 @@ set @age = datediff(YEAR,@bymd,@rymd) - @offset
 --////////////////////////////////////////////////////////////////////
 if day(@rymd) < day(@bymd) set @offset = 1
 else set @offset = 0
-set @age = @age+((datediff(MONTH,@bymd,@rymd)-@offset-(@age*12))/100)
+set @age = @age+((datediff(MONTH,@bymd,@rymd)-@offset-(@age*12)/100))
 return @age
 end
--- src/functions/irisage.sql
+++ src/functions/irisage.sql
@@ -6,14 +6,20 @@ LANGUAGE OBJECTSCRIPT
 {
  set ymd1 = $zdate(bdate,8)
  set ymd2 = $zdate(cdate,8)
- set age = (ymd2-ymd1)\10000
+ set ydiff = ymd2-ymd1
+ set age = ydiff\10000
+ 
+ if ydiff < 0 {
+   set age = age - 1	
+ }
+ 
  if ($system.SQL.Functions.DAYOFMONTH(cdate) < $system.SQL.Functions.DAYOFMONTH(bdate)) {
    set offset = 1
  }
  else {
    set offset = 0
  }
- set age = age+(($system.SQL.Functions.DATEDIFF("MONTH",bdate,cdate)-offset-(age*12))/100)
+ set age = age+(($system.SQL.Functions.DATEDIFF("MONTH",bdate,cdate)-offset-(age*12)/100))
  quit age
 }
 go
